f3480c3887ed4e5d6afd487c7856a39f0581d491
Sashko <sashkoace@gmail.com>
2021-01-21 14:35:49 +0200

    added counter, jump and dead

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,46 @@
 'use strict'
 
-const character = document.querySelector('.game__character');
+const character = document.querySelector('.game__character'),
+    block = document.querySelector('.game__block');
 
 window.addEventListener('keydown', (e) => {
-    console.log(e);
+    if (e.key == " ") {
+        jump(e);
+    }
 });
+
+function jump(e) {
+    character.classList.add('game__character--active');
+
+    setTimeout(() => {
+        character.classList.remove('game__character--active');
+    }, 600);
+}
+
+const counter = document.querySelector('.game__counter-current');
+let i = 0
+
+const setCounter = setInterval(() => {
+    counter.innerHTML = `Current ${i}`;
+    i++;
+}, 100);
+
+const score = document.querySelector('.game__score'),
+    scoreText = document.querySelector('.game__score-text');
+
+const checkDead = setInterval(() => {
+    const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+
+    if (blockLeft <= 150 && blockLeft >= 100 && characterTop >= 250) {
+        dead();
+    }
+
+}, 10);
+
+function dead() {
+    clearInterval(setCounter);
+    block.classList.add('game__block--dead');
+    score.classList.add('game__score--active');
+    scoreText.innerHTML = `Your score = ${--i}`;
+}
